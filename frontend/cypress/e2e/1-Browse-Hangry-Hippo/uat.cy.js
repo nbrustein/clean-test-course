2129ef378f7f1c9ec3a9c3a2854c9dcf78b99542
Nate Brustein <nbrustein@gmail.com>
2025-09-04 10:04:15 -0400

    lesson6

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,20 @@
 describe('Shows Appeteasers', () => {
   it('Connect to Dev Server', () => {
-    cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/');
+    // cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/');
+    cy.visit('http://localhost:3000/');
   });
   it('selects Handhelds', () => {
     //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
     //TODO: Add a test to check Appeteasers
   });
 });
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = 'https://hangryhippo-api.quantic.host';
+// export const API_URL = 'https://hangryhippo-api.quantic.host';
+export const API_URL = 'http://localhost:8000';
